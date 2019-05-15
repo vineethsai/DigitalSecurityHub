47fa838c49277cb919c4111bae51259161f656fa
Quinn Zapata <qzapata@uw.edu>
2019-05-15 06:19:45 -0700

    fix broken db

--- DigitalSecurityHub/DigitalSecurityHub/settings.py
+++ DigitalSecurityHub/DigitalSecurityHub/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'orders.apps.OrdersConfig',
     'products.apps.ProductsConfig',
     'shop.apps.ShopConfig',
